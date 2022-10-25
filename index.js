3fc29a2a098affa001d2edb061e7889c2b8ff70f
Mohammed Atif Ansari <atifa7747@gmail.com>
2022-10-25 19:09:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const containerE1 = document.querySelector(".container");
+
+const btnE1 = document.querySelector(".btn");
+
+
+const popupCOntainerE1 = document.querySelector(".popup-container");
+
+const closeIconE1= document.querySelector(".close-icon");
+
+btnE1.addEventListener("click", ()=>{
+    containerE1.classList.add("active");
+
+    popupCOntainerE1.classList.remove("active");
+});
+
+
+closeIconE1.addEventListener("click", ()=>{
+    containerE1.classList.remove("active");
+
+    popupCOntainerE1.classList.add("active");
+});
